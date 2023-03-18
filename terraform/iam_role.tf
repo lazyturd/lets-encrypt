9dcc2d8b777ec1b615841f1ae7d91047208a6c74
Ayo <ayorexyshittu@gmail.com>
2023-03-18 09:00:15 +0100

    tf

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -44,7 +44,7 @@ module "cert_manager_irsa_role" {
     version = "5.2.0"
 
     role_name = "cert-manager"
-    attache_cert_manager_policy = true
+    attach_cert_manager_policy = true
     cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/Z0600261OY5QAKYH2M34"]
 
     oidc_providers = {

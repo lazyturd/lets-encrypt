cfdb0485c4058d4868f40681d8f48154e6bf6210
Ayo <ayorexyshittu@gmail.com>
2023-03-18 10:56:37 +0100

    hh

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,9 +19,9 @@ provider "aws" {
   region  = "${var.region}"
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_id
+# }
 
 provider "helm" {
   kubernetes {

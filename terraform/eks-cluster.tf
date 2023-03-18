37650e0a2b6c983df982a2aca01a44267e39d7d3
Ayo <ayorexyshittu@gmail.com>
2023-03-18 10:40:41 +0100

    u

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
     cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
+    cluster_version = "1.25"
 
     cluster_endpoint_public_access  = true
 
@@ -37,5 +37,19 @@ module "eks" {
 
             instance_types = ["t2.micro"]
         }
+    }
+
+    node_security_group_additional_rules = {
+
+        dns_all = {
+            description = "DNS ALL"
+            protocol = "-1"
+            from_port = 53
+            to_port = 53
+            type = "egress"
+            cidr_blocks = ["0.0.0.0/0"]
+            ipv6_cidr_blocks = ["::/0"]
+        }
+
     }
 }
--- terraform/helm.tf
+++ terraform/helm.tf
@@ -29,9 +29,9 @@ resource "helm_release" "cert_manager" {
 }
 
 resource "helm_release" "external_dns" {
-  name       = "external_dns"
+  name       = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "external_dns"
+  chart      = "external-dns"
 
   create_namespace = true
   namespace = "kube-system"
